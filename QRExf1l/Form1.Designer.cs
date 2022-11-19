68aed0fbee2fb600ccca16402a78f531f4047c89
mvc1009 <48022811+mvc1009@users.noreply.github.com>
2022-11-19 14:22:57 +0100

    adding file splitting

--- QRExf1l/Form1.Designer.cs
+++ QRExf1l/Form1.Designer.cs
@@ -29,18 +29,23 @@
         private void InitializeComponent()
         {
             this.buttonQR = new System.Windows.Forms.Button();
-            this.picQR = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
             this.textQR = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.ofdFile = new System.Windows.Forms.OpenFileDialog();
             this.btnFile = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.picQR)).BeginInit();
+            this.textStatus = new System.Windows.Forms.Label();
+            this.numericSleep = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericQRLength = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericSleep)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericQRLength)).BeginInit();
             this.SuspendLayout();
             // 
             // buttonQR
             // 
-            this.buttonQR.Location = new System.Drawing.Point(383, 418);
+            this.buttonQR.Location = new System.Drawing.Point(409, 439);
             this.buttonQR.Name = "buttonQR";
             this.buttonQR.Size = new System.Drawing.Size(75, 23);
             this.buttonQR.TabIndex = 0;
@@ -48,21 +53,11 @@
             this.buttonQR.UseVisualStyleBackColor = true;
             this.buttonQR.Click += new System.EventHandler(this.buttonQR_Click);
             // 
-            // picQR
-            // 
-            this.picQR.Location = new System.Drawing.Point(31, 77);
-            this.picQR.Name = "picQR";
-            this.picQR.Size = new System.Drawing.Size(427, 332);
-            this.picQR.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
-            this.picQR.TabIndex = 1;
-            this.picQR.TabStop = false;
-            this.picQR.Click += new System.EventHandler(this.picQR_Click);
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(28, 21);
+            this.label1.Location = new System.Drawing.Point(191, 17);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(86, 24);
             this.label1.TabIndex = 2;
@@ -71,7 +66,7 @@
             // 
             // textQR
             // 
-            this.textQR.Location = new System.Drawing.Point(120, 420);
+            this.textQR.Location = new System.Drawing.Point(146, 441);
             this.textQR.Name = "textQR";
             this.textQR.Size = new System.Drawing.Size(257, 20);
             this.textQR.TabIndex = 3;
@@ -80,12 +75,12 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(393, 32);
+            this.label2.Location = new System.Drawing.Point(274, 25);
             this.label2.Name = "label2";
             this.label2.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.Size = new System.Drawing.Size(76, 13);
             this.label2.TabIndex = 4;
-            this.label2.Text = "by mvc1009";
+            this.label2.Text = "by @mvc1009";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // ofdFile
@@ -95,7 +90,7 @@
             // 
             // btnFile
             // 
-            this.btnFile.Location = new System.Drawing.Point(31, 418);
+            this.btnFile.Location = new System.Drawing.Point(57, 439);
             this.btnFile.Name = "btnFile";
             this.btnFile.Size = new System.Drawing.Size(83, 23);
             this.btnFile.TabIndex = 6;
@@ -103,20 +98,94 @@
             this.btnFile.UseVisualStyleBackColor = true;
             this.btnFile.Click += new System.EventHandler(this.btnFile_Click);
             // 
+            // textStatus
+            // 
+            this.textStatus.AutoSize = true;
+            this.textStatus.Location = new System.Drawing.Point(410, 475);
+            this.textStatus.Name = "textStatus";
+            this.textStatus.Size = new System.Drawing.Size(0, 13);
+            this.textStatus.TabIndex = 7;
+            this.textStatus.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // numericSleep
+            // 
+            this.numericSleep.Location = new System.Drawing.Point(418, 409);
+            this.numericSleep.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericSleep.Name = "numericSleep";
+            this.numericSleep.Size = new System.Drawing.Size(66, 20);
+            this.numericSleep.TabIndex = 9;
+            this.numericSleep.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(347, 411);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Sleep (ms)";
+            this.label3.Click += new System.EventHandler(this.label3_Click_1);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(53, 411);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "QR length(chars)";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // numericQRLength
+            // 
+            this.numericQRLength.Location = new System.Drawing.Point(146, 409);
+            this.numericQRLength.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericQRLength.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericQRLength.Name = "numericQRLength";
+            this.numericQRLength.Size = new System.Drawing.Size(68, 20);
+            this.numericQRLength.TabIndex = 12;
+            this.numericQRLength.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numericQRLength.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(476, 450);
+            this.ClientSize = new System.Drawing.Size(541, 474);
+            this.Controls.Add(this.numericQRLength);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericSleep);
+            this.Controls.Add(this.textStatus);
             this.Controls.Add(this.btnFile);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textQR);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.picQR);
             this.Controls.Add(this.buttonQR);
             this.Name = "Form1";
-            this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.picQR)).EndInit();
+            this.Text = "QRExf1l";
+            ((System.ComponentModel.ISupportInitialize)(this.numericSleep)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericQRLength)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -125,12 +194,16 @@
         #endregion
 
         private System.Windows.Forms.Button buttonQR;
-        private System.Windows.Forms.PictureBox picQR;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textQR;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.OpenFileDialog ofdFile;
         private System.Windows.Forms.Button btnFile;
+        private System.Windows.Forms.Label textStatus;
+        private System.Windows.Forms.NumericUpDown numericSleep;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericQRLength;
     }
 }
 
--- QRExf1l/Form1.cs
+++ QRExf1l/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using QRCoder;
@@ -12,20 +13,32 @@ using QRCoder;
 
 namespace QRExf1l
 {
+
     public partial class Form1 : Form
-    { 
+    {
+        private PictureBox p;
+        private List<Image> QRImageList;
+        private System.Windows.Forms.Timer timer;
+        private int count;
 
         public Form1()
         {
             InitializeComponent();
+            p = new PictureBox
+            {
+                Name = "QRImageBox",
+                Size = new Size(400, 400),
+                SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+            };
+            QRImageList = new List<Image>();
+            timer = new System.Windows.Forms.Timer();
+            timer.Interval = Convert.ToInt32(numericSleep.Value);
+            timer.Tick += new EventHandler(timer_Tick);
+            count = 0;
+            this.Controls.Add(p);
         }
-
+ 
         private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void picQR_Click(object sender, EventArgs e)
         {
 
         }
@@ -42,10 +55,31 @@ namespace QRExf1l
 
         private void buttonQR_Click(object sender, EventArgs e)
         {
-            QRCodeGenerator qrGenerator = new QRCodeGenerator();
-            QRCodeData qrCodeData = qrGenerator.CreateQrCode(textQR.Text, QRCodeGenerator.ECCLevel.Q);
-            QRCode qrCode = new QRCode(qrCodeData);
-            picQR.Image = qrCode.GetGraphic(5);
+
+
+            string text = System.IO.File.ReadAllText(textQR.Text);
+            string vartext = "";
+
+
+            
+
+            for (int i=0; i < text.Length; i=i+ Convert.ToInt32(numericQRLength.Value)) {
+                if( (text.Length - i <= numericQRLength.Value))
+                {
+                    vartext = text.Substring(i);
+                }
+                else
+                {
+                    vartext = text.Substring(i, Convert.ToInt32(numericQRLength.Value));
+                }
+                Console.WriteLine(vartext);
+                QRCodeGenerator qrGenerator = new QRCodeGenerator();
+                QRCodeData qrCodeData = qrGenerator.CreateQrCode(vartext, QRCodeGenerator.ECCLevel.Q);
+                QRCode qrCode = new QRCode(qrCodeData);
+                QRImageList.Add(qrCode.GetGraphic(5));
+            }
+            timer.Start();
+            
         }
 
         private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
@@ -60,6 +94,55 @@ namespace QRExf1l
             {
                 textQR.Text = ofd.FileName;
             }
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void changeImage(Image pic, EventArgs e)
+        {
+            p.Image = pic;
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            p.Image = QRImageList[count];
+            if (count < QRImageList.Count -1)
+            {
+                count++;
+            }
+            else
+            {
+                Console.WriteLine("[!] End");
+                timer.Stop();
+            }
+        }
+
+        private void domainUpDown1_SelectedItemChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
 
         }
     }
